234b1fa24054b164df891aaa80de635694aa6441
martha <meeney03@gmail.com>
2024-01-13 14:09:13 +0100

    components recreated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './header/shopping-list/shopping-list/shopping-list.component';
-import { RecipeListComponent } from './header/recipe-book/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './header/recipe-book/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './header/recipe-book/recipe-detail/recipe-detail.component';
-import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, ShoppingListComponent, RecipeListComponent, RecipeItemComponent, RecipeDetailComponent, ShoppingListEditComponent
+    AppComponent, 
+    HeaderComponent, 
+    RecipesComponent, 
+    RecipeListComponent, 
+    RecipeItemComponent, 
+    RecipeDetailComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
