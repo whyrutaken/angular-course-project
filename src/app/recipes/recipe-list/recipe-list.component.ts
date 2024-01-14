a771075aff76c2b23a3c3b60ed53652d74c309fc
martha <meeney03@gmail.com>
2024-01-14 12:14:18 +0100

    recipes dynamically added to recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,6 +8,7 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes: Recipe[] = [
-    new Recipe("Test Spaghetti", "This is a test spaghetti with a description.", "https://ih1.redbubble.net/image.5218369573.9590/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg" )
+    new Recipe("Test Spaghetti", "This is a test spaghetti with a description.", "https://ih1.redbubble.net/image.5218369573.9590/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg"),
+    new Recipe("Test Nokedli", "I am a Nokedli description.", "https://static.streetkitchen.hu/live/uploads/2017/09/tojasos-nokedli-1-scaled.jpg")
   ];
 }
