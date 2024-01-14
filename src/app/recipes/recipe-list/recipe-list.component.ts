47e23dd910d9e374e86d995b065c421125eca497
martha <meeney03@gmail.com>
2024-01-14 12:04:33 +0100

    put recipe in recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe("Test Spaghetti", "This is a test spaghetti with a description.", "https://ih1.redbubble.net/image.5218369573.9590/flat,750x,075,f-pad,750x1000,f8f8f8.u1.jpg" )
+  ];
 }
